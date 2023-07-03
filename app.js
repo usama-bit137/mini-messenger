220c4ef08fbc7444d0061e3281a838e6025fcc2f
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-03 23:08:56 +0100

    Set up pug as a view template. Set up static files. Implemented JSON parser.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+  res.status(200).render('base', {
+    title: 'Top view',
+  });
+});
 
 module.exports = app;
