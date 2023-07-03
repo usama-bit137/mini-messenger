ac288edfd7b6812749b29f1e74bb1577b849d2da
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-04 00:21:51 +0100

    Server connection message changed.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
-
 const app = require('./app');
 
 const port = 3000;
 app.listen(port, () => {
   console.log(
-    `Server started at ${new Date().toJSON().slice(11, -2)} on port: ${port}`
+    `App listening for HTTP requests on port ${port}\nServer started at ${new Date()
+      .toJSON()
+      .slice(11, -2)}`
   );
 });
