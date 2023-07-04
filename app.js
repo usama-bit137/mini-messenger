11cfe00b346e811b285de67fda7b03bf1360a95b
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-04 19:08:23 +0100

    Api routes implemented from messageRouter.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const viewsRouter = require('./routes/viewsRoutes');
+const messagesRouter = require('./routes/messageRoutes');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -10,5 +11,9 @@ app.use(express.json());
 
 // TEMPLATE ROUTES
 app.use('/', viewsRouter);
+
+// API ROUTES:
+
+app.use('/api/v1/messages', messagesRouter);
 
 module.exports = app;
