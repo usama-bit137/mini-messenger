e4760e8d1786f897f5b0858e89f162af59358e5d
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-04 22:27:04 +0100

    Morgan HTTP messages added in development node environment.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const viewsRouter = require('./routes/viewsRoutes');
 const messagesRouter = require('./routes/messageRoutes');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
