7e1f164933ceba38db9f3d71e10828d15240b4d3
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-05 02:25:17 +0100

    Removed all routers except the ones for getting all messages and posting a message.

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,12 +2,6 @@ const express = require('express');
 const controller = require('../controllers/messageController');
 
 const router = express.Router();
-
 router.route('/').get(controller.getAllMessages).post(controller.postMessage);
-// router
-//   .route('/:id')
-//   .get(controller.getMessage)
-//   .patch(controller.updateMessage)
-//   .delete(controller.deleteMessage);
 
 module.exports = router;
