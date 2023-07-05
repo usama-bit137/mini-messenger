c8b3addaeefb68c114cc7633ae55bed88ad1f44c
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-05 15:20:35 +0100

    Added database calls for messages in the DB. Routing fixed.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,14 @@
 const Messages = require('../models/messageModel');
 
+exports.getAllMessages = async (req, res) => {
+  const messages = await Messages.find();
+  res.status(200).render('overview', {
+    title: 'Chat',
+    messages,
+  });
+};
+
 exports.postMessage = async (req, res) => {
   await Messages.create(req.body);
-  res.redirect('/');
+  res.redirect('/overview');
 };
