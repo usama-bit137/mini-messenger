4c1d091ce89a1b9251afcf4aa34ada9d2a2ed6cb
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-05 15:20:58 +0100

    Routing fixed.

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -3,12 +3,8 @@ const controller = require('../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).render('base', {
-    title: 'Your new favourite messaging app',
-  });
-});
-
-router.post('/', controller.postMessage);
+router
+  .get('/overview', controller.getAllMessages)
+  .post('/', controller.postMessage);
 
 module.exports = router;
