de1429f12a7bbed45093e2687c7a8dafe2430e2d
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-05 02:19:43 +0100

    Changed the api/ routes, as it is overkill. Added body-parser node module to allow for form submission.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const messageRouter = require('./routes/messageRoutes');
 const viewsRouter = require('./routes/viewsRoutes');
-const messagesRouter = require('./routes/messageRoutes');
 
 const app = express();
 
@@ -11,12 +12,13 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+// DATABASE ROUTES
+app.use('/messages', messageRouter);
 
 // TEMPLATE ROUTES
 app.use('/', viewsRouter);
-
-// API ROUTES
-app.use('/api/v1/messages', messagesRouter);
 
 module.exports = app;
