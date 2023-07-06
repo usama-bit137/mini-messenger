c83b1308dbcf688a5fda601f1511c2520be2eef2
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-06 18:08:05 +0100

    Custom form validation implemented instead of required field.

--- public/scripts/formValidation.js
+++ public/scripts/formValidation.js
@@ -1,14 +1,19 @@
 const user = document.getElementById('user');
-// const text = document.getElementById('text');
+const text = document.getElementById('text');
 const errorElement = document.getElementById('error');
 
 document.getElementById('form').addEventListener('submit', (e) => {
   const messages = [];
-  if (user.value === '' || user.value == null)
-    messages.push('Name is required');
+  if (user.value === '' || user.value === null) messages.push('username');
+  if (text.value === '' || text.value === null) messages.push('text');
 
   if (messages.length > 0) {
     e.preventDefault();
-    errorElement.innerText = messages.join(', ');
+    errorElement.innerText = `${messages.join(' and ')} required`;
+    errorElement.classList.add('on-error');
+    setTimeout(() => {
+      errorElement.innerText = '';
+      errorElement.classList.remove('on-error');
+    }, 2000);
   }
 });
